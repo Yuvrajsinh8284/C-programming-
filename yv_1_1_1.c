b6a71d2cea8bfbcfc32e6609812c4e4ebadc8bf7
Yuvrajsinh8284 <yuvrajsinhvala8284@gmail.com>
2025-08-26 15:30:44 +0530

    Add files via upload

---
+++ yv_1_1_1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+  int a,b,c;
+  printf("enter the value of a\n");
+  scanf("%d\n",&a);
+  printf("enter the value of b\n");
+  scanf("%d\n",&b);
+  c=a+b;
+  printf("the sum is %d\n",c);
+}
