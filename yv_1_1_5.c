65ff20b4debbfb167bcba6fce8990754cb0f7b18
Yuvrajsinh8284 <yuvrajsinhvala8284@gmail.com>
2025-08-28 16:16:07 +0530

    Add files via upload

---
+++ yv_1_1_5.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+  int a,b;
+  printf("enter the value of a\n");
+  scanf("%d\n",&a);
+  printf("enter the value of b\n");
+  scanf("%d\n",&b);
+  printf("addition is %d\n",a+b);
+  printf("subtraction is %d\n",a-b);
+    printf("multiplication is %d\n",a*b);
+  printf("division is %d\n",a/b);
+}
