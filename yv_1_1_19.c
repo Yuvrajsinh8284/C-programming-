23fc2fd005bcb82fcaf45bfca389fece20f0ba35
Yuvrajsinh8284 <yuvrajsinhvala8284@gmail.com>
2025-09-08 19:03:14 +0530

    Add files via upload

---
+++ yv_1_1_19.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+  float r;
+  printf("enter the radius of circle \n");
+  scanf("%f\n",&r);
+  printf(" area of circle is %f\n",3.14*r*r);
+  return 0;
+}
---
+++ yv_1_1_20.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+  int main()
+  {
+    float h, l;
+    printf("enter  base and  triangle's height\n");
+    scanf("%f%f",&h,&l);
+    printf(" area of triangle is %f\n", 1/2.0*h*l);
+    return 0;
+  }
